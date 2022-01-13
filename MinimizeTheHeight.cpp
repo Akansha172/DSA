91ba41acc11d176860b093cad5de25440eefadd3
Mahesh Gaikwad <gaikwadmahesh777g@gmail.com>
2022-01-13 12:29:43 +0530

    minimize the height solution

--- MinimizeTheHeight.cpp
+++ MinimizeTheHeight.cpp
@@ -10,19 +10,22 @@ using namespace std;
 class Solution {
   public:
     int getMinDiff(int arr[], int n, int k) {
-        int small_element,large_element;
-        for(int i=0;i<n;i++){
-            if(arr[i]-k < 0){
-                arr[i]+=k;
-            }else
-            {
-                arr[i]-=k;
-            }
+        sort(arr, arr+n);
+        int diff = arr[n-1]- arr[0];
+        
+        int maxi = arr[n-1];
+        int mini = arr[0];
+        
+        for(int i=1; i<n; i++)
+        {
+            if(arr[i]-k<0)
+            continue;
+            
+            mini = min(arr[0]+k, arr[i]-k);
+            maxi = max(arr[n-1]-k, arr[i-1]+k);
+            diff = min(diff, maxi-mini);
         }
-       small_element = *min_element(arr,arr+n);
-       large_element = *max_element(arr,arr+n);
-       int ans =  large_element - small_element -1;
-        return ans;
+        return diff;
     }
 };
 
